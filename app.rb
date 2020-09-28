fe825c59a9354d317251a6a2628016b5e1c50727
Keesha Hargrove <kharg14@gmail.com>
2020-09-28 18:16:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    #   get '/' do
-    #   erb :index
-    # end
+      get '/' do
+      erb :index
+    end
  
     get "/info" do
       erb :dogs
