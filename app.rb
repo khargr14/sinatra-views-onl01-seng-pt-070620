399e7c7f82fc476e0d846cfd05ee3a91db55f683
Keesha Hargrove <kharg14@gmail.com>
2020-09-28 17:46:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,30 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
+    # get '/' do
+    #   erb :index
+    # end
+ 
+    # get "/info" do
+    #   erb :dogs
+#     end
+get '/' do
+		erb :index
 	end
-end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
+
+
+
+
+ end
